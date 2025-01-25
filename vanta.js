fad2a7da576891a574722bb41d493f1a4dc97828
jatinparmar24 <jatinparmar2424@gmail.com>
2025-01-25 22:57:26 +0530

    not border

--- vanta.js
+++ vanta.js
@@ -1,18 +1,18 @@
-VANTA.FOG({
-  el: "#divcont",
-  mouseControls: true,
-  touchControls: true,
-  gyroControls: false,
-  minHeight: 200.00,
-  minWidth: 200.00,
-  highlightColor: 0x49919a,
-  midtoneColor: 0x532abb,
-  lowlightColor: 0x2f18f4,
-  baseColor: 0xcaccf7,
-  blurFactor: 0.51,
-  speed: 2.30,
-  zoom: 0.90
-})
+// VANTA.FOG({
+//   el: "#divcont",
+//   mouseControls: true,
+//   touchControls: true,
+//   gyroControls: false,
+//   minHeight: 200.00,
+//   minWidth: 200.00,
+//   highlightColor: 0x49919a,
+//   midtoneColor: 0x532abb,
+//   lowlightColor: 0x2f18f4,
+//   baseColor: 0xcaccf7,
+//   blurFactor: 0.51,
+//   speed: 2.30,
+//   zoom: 0.90
+// })
 
 
 
