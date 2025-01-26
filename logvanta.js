9ff92dc7bea7e2d207a37160099574a40aa2989d
jatinparmar24 <jatinparmar2424@gmail.com>
2025-01-26 15:05:11 +0530

    edit book form

--- logvanta.js
+++ logvanta.js
@@ -1,16 +1,21 @@
-VANTA.HALO({
+VANTA.BIRDS({
   el: "#logbody",
   mouseControls: true,
   touchControls: true,
   gyroControls: false,
   minHeight: 200.00,
   minWidth: 200.00,
-  baseColor: 0x14ef91,
-  backgroundColor: 0x8a60f4,
-  amplitudeFactor: 2.80,
-  xOffset: 0.11,
-  yOffset: -0.03,
-  size: 1.10
+  scale: 1.00,
+  scaleMobile: 1.00,
+  backgroundColor: 0xd0d0e,
+  color1: 0xedc62a,
+  color2: 0x62be5,
+  birdSize: 0.90,
+  wingSpan: 23.00,
+  speedLimit: 7.00,
+  separation: 46.00,
+  alignment: 50.00,
+  cohesion: 77.00
 })
 
 
